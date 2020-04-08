dbfa3bcaa11526fcbed0d58646b86ee425d1ff78
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-08 18:04:56 -0400

    post request works

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.use(logger);
 
 server.use(express.json());
 
-server.use("/api/users", userRouter);
+server.use("/api/user", userRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,13 +1,32 @@
 const express = require("express");
-
+const Users = require("./userDb");
 const router = express.Router();
 
 router.post("/", (req, res) => {
   // do your magic!
+  const { name } = req.body;
+  Users.insert(req.body)
+    .then((newUser) => {
+      if (!name) {
+        res.status(400).json({ error: "missing name" });
+      } else if (!newUser) {
+        res.status(400).json({ error: "input user content" });
+      } else if (newUser) {
+        res.status(401).json(newUser);
+      } else {
+        res.status(500).json({ err: "big mess up" });
+      }
+    })
+    .catch((err) => {
+      console.log("you messed up adding a new post", err);
+    });
 });
 
 router.post("/:id/posts", (req, res) => {
   // do your magic!
+  const newPost = req.body;
+  const { text, user_id } = req.body;
+  Users.insert();
 });
 
 router.get("/", (req, res) => {
