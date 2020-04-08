03f02380eba9aed5d88ed62028b70d0a6803a89e
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-08 18:49:02 -0400

    endpoints almost done

--- users/userRouter.js
+++ users/userRouter.js
@@ -26,19 +26,69 @@ router.post("/:id/posts", (req, res) => {
   // do your magic!
   const newPost = req.body;
   const { text, user_id } = req.body;
-  Users.insert();
+  Users.insert(newPost)
+    .then((posts) => {
+      if (!text) {
+        res.status(400).json({ message: "please insert text" });
+      } else if (!user_id) {
+        res.status(400).json({ message: "userID is not specified" });
+      } else if (posts) {
+        res.status(201).json(posts);
+      } else {
+        res.status(500).json({ message: "error sending data to database" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 router.get("/", (req, res) => {
   // do your magic!
+  Users.get(req.query)
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.log("error getting data from database", err);
+      res.status(500).json({ error: "error retrieving data" });
+    });
 });
 
 router.get("/:id", (req, res) => {
   // do your magic!
+  Users.getById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else if (!post) {
+        res.status(404).json({ message: "post could not be found" });
+      } else {
+        res.status(500).json({ error: "post info could not be retrieved" });
+      }
+    })
+    .catch((err) => {
+      console.log("error getting via ID", err);
+    });
 });
 
 router.get("/:id/posts", (req, res) => {
   // do your magic!
+  Users.getUserPosts()
+    .then((posts) => {
+      if (posts) {
+        res.status(200).json(posts);
+      } else if (!posts) {
+        res.status(404).json({ error: "posts with that ID dont exists" });
+      } else {
+        res
+          .status(500)
+          .json({ error: "The comments information could not be retrieved." });
+      }
+    })
+    .catch((err) => {
+      console.log("error getting user posts", err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
